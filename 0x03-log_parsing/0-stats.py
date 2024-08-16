ebc438f8b8b8c846bd994912d788a4dfac9b5391
olawalesegun <olawalesegun0@gmail.com>
2024-08-16 07:30:14 +0300

    a script that reads stdin line by line and computes metrics:
    Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size> (if the format is not this one, the line must be skipped)
    After every 10 lines and/or a keyboard interruption (CTRL + C), print these statistics from the beginning:
    Total file size: File size: <total size>
    where <total size> is the sum of all previous <file size> (see input format above)
    Number of lines by status code:
    possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
    if a status code doesn’t appear or is not an integer, don’t print anything for this status code
    format: <status code>: <number>
    status codes should be printed in ascending order

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This module reads stdin line by line and computes metrics"""
+
+import sys
+import re
+
+file_size = 0
+status_dict = {"200": 0, "301": 0, "400": 0, "401": 0,
+               "403": 0, "404": 0, "405": 0, "500": 0}
+line_number = 0
+
+try:
+    for line in sys.stdin:
+        line = line.strip()
+        line_parts = line.split()
+
+        try:
+            status_code = line_parts[-2]
+            if status_code in status_dict:
+                status_dict[status_code] += 1
+        except Exception:
+            pass
+        try:
+            file_size += int(line_parts[-1])
+        except Exception:
+            pass
+        line_number += 1
+
+        if line_number == 10:
+            print("File size: {}".format(file_size))
+            for key, value in sorted(status_dict.items()):
+                if value > 0:
+                    print("{}: {}".format(key, value))
+            line_number = 0
+
+except KeyboardInterrupt:
+    pass
+
+print("File size: {}".format(file_size))
+for key, value in sorted(status_dict.items()):
+    if value > 0:
+        print("{}: {}".format(key, value))
+
