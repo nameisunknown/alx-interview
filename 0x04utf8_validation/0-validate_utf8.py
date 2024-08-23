7c4b345632a0c8025c157060401fca4668d4d729
nameisunknown <orangekapital@gmail.com>
2024-08-23 07:10:45 +0300

    debugging issues resolved

--- 0x04utf8_validation/0-validate_utf8.py
+++ 0x04utf8_validation/0-validate_utf8.py
@@ -7,14 +7,14 @@ def validUTF8(data):
     # Number of bytes in the current UTF-8 character
     num_bytes = 0
 
-    # Masks to check the significant bits of each byte
+    # Masking to check the significant bits of each byte
     mask1 = 1 << 7  # 10000000
     mask2 = 1 << 6  # 01000000
 
     for num in data:
         mask = 1 << 7
         if num_bytes == 0:
-            # Determine the number of bytes in the UTF-8 character
+            # Determining the number of bytes in the UTF-8 character
             while mask & num:
                 num_bytes += 1
                 mask = mask >> 1
@@ -23,7 +23,7 @@ def validUTF8(data):
             if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            # Check that the next byte starts with '10'
+            # Checking that the next byte starts with '10'
             if not (num & mask1 and not (num & mask2)):
                 return False
         num_bytes -= 1
