6096b8a92d14f287e0acd43bf650080c1b3bdeee
nameisunknown <orangekapital@gmail.com>
2024-09-28 05:30:41 +0300

    island perimeter with Algorithm practice

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''calculate the perimeter of an island'''
+
+
+def island_perimeter(grid):
+    '''calculate the perimeter of an island (graph)'''
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0]) if rows > 0 else 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+
+    return perimeter
